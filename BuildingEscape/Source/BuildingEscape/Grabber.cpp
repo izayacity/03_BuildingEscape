9d228deaeaedac4161d78f79312716874ea60b49
izayacity <xyang42@ucsc.edu>
2017-01-11 16:36:14 -0800

    Add ray-cast to the pawn character

---
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -0,0 +1,65 @@
+// Copyright Ben Tristem 2016.
+
+#include "BuildingEscape.h"
+#include "Grabber.h"
+
+
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG (LogTemp, Warning, TEXT ("Grabber reporting for duty!"));
+	
+}
+
+
+// Called every frame
+void UGrabber::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+{
+	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+
+	// Get player view point this trick
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+
+	GetWorld ()->GetFirstPlayerController ()->GetPlayerViewPoint (
+		OUT PlayerViewPointLocation,
+		OUT PlayerViewPointRotation
+	);
+
+	UE_LOG (LogTemp, Warning, TEXT ("Location: %s, Position: %s"), 
+		*PlayerViewPointLocation.ToString(), 
+		*PlayerViewPointRotation.ToString()
+	);
+	
+	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
+
+	// Draw a red trace in the world to visual
+	DrawDebugLine (
+		GetWorld (),
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FColor (255, 0, 0),
+		false,
+		0.f,
+		0.f,
+		10.f
+	);
+
+	// ray-cast out to reach distance
+
+	// see what we hit
+}
+
